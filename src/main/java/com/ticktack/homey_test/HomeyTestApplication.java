d602ed2fbb10c6bd0d11f89c0ceeeaaa8e184062
YesolLee421 <dldpthf421@gmail.com>
2022-08-02 17:40:20 +0900

    initial commit

---
+++ src/main/java/com/ticktack/homey_test/HomeyTestApplication.java
@@ -0,0 +1,13 @@
+package com.ticktack.homey_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeyTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticktack/homey_test/HomeyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticktack.homey_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
