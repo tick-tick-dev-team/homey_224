4794cefb0517d58836a1cf15f7fb269ae08c6d81
YesolLee421 <dldpthf421@gmail.com>
2022-08-09 17:39:34 +0900

    sprint 웹 개발 방법 3가지 연습 (static, view, api)

--- src/main/java/com/ticktack/homey_test/Controller/HelloController.java
+++ src/main/java/com/ticktack/homey_test/Controller/HelloController.java
@@ -3,6 +3,8 @@ package com.ticktack.homey_test.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,6 +13,43 @@ public class HelloController {
 	public String hello(Model model) {
 		model.addAttribute("data", "예솔");
 		return "hello";
+	}
+	
+	@GetMapping("hello-mvc")
+	public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello-template";
+	}
+	
+	// 문자 그대로
+	@GetMapping("hello-string")
+	@ResponseBody
+	public String helloString(@RequestParam(value = "name", required = false) String name) {
+		
+		return "hello " + name;
+	}
+	
+	// data 전달
+	@GetMapping("hello-api")
+	@ResponseBody
+	public Hello helloAPI(@RequestParam(value = "name", required = false) String name) {
+		
+		Hello hello = new Hello();
+		hello.setName(name);
+		
+		return hello;
+	}
+	
+	static class Hello {
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
 	}
 
 }
